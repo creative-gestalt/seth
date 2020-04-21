6a91c9f2032f8d7429b7093404c2df43c265b69d
lordweasel <lordweasel6@gmail.com>
2020-04-21 14:48:28 -0600

    cleaned up unused imports and fixed color of line separators

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { setString } from 'tns-core-modules/application-settings';
+import { ThemeService } from '~/app/shared/services/theme.service';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import * as app from 'tns-core-modules/application';
-import { setString } from 'tns-core-modules/application-settings';
-import { Page } from 'tns-core-modules/ui/page/page';
-import { ThemeService } from '~/app/shared/services/theme.service';
 
 @Component({
     selector: 'Settings',
@@ -12,7 +11,6 @@ import { ThemeService } from '~/app/shared/services/theme.service';
 export class SettingsComponent implements OnInit {
 
     constructor(
-        private _page: Page,
         private _theme: ThemeService
     ) {
     }
